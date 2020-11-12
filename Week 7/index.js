3a7b2a0cc53445f9061c82537fa585b4dac6ece3
Laura J <laura.jakseboge@gmail.com>
2020-11-12 13:32:11 +0100

    website added;

--- Week 7/index.js
+++ Week 7/index.js
@@ -1,5 +1,6 @@
 /*
 DESCRIPTION:
+https://scrimba.com/scrim/cwweagtn
 In this challenge you are a magician that tells your audience that by pressing only
 the first square on the left, you can change ALL 9 squares on the grid to be yellow.
 Write a function using the .forEach() method that will do just that.

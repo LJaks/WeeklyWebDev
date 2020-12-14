1a291c7b724e643b7b093608d0a6862ef79a2a83
Laura J <laura.jakseboge@gmail.com>
2020-12-14 09:31:39 +0100

    Time interval changes;

--- JavaScriptmas2020/12-Valid Time/main.js
+++ JavaScriptmas2020/12-Valid Time/main.js
@@ -1,62 +1,60 @@
 function validTime(str) {
-    //  write code here.
-    let num = str.split(":")
-    if (parseInt(num[0]) < 0 || parseInt(num[0]) > 24){
-        console.log(`wrong hours ${parseInt(num[0])}`)
-        return false;
-    }
-    if (parseInt(num[1]) < 0 || parseInt(num[1]) >= 60) {
-       console.log(`wrong minutes ${parseInt(num[1])}`)
-       return false;
-    }
-    return true;
+  //  write code here.
+  let num = str.split(":");
+  if (parseInt(num[0]) < 0 || parseInt(num[0]) > 23) {
+    console.log(`wrong hours ${parseInt(num[0])}`);
+    return false;
+  }
+  if (parseInt(num[1]) < 0 || parseInt(num[1]) > 59) {
+    console.log(`wrong minutes ${parseInt(num[1])}`);
+    return false;
+  }
+  return true;
 }
 
-
-
 /**
-* Test Suite 
-*/
-describe('validTime()', () => {
-    it('returns true for valid time', () => {
-        // arrange
-        const str = '13:58';
-        
-        // act
-        const result = validTime(str);
+ * Test Suite
+ */
+describe("validTime()", () => {
+  it("returns true for valid time", () => {
+    // arrange
+    const str = "13:58";
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(true);
-    });
+    // act
+    const result = validTime(str);
 
-    it('returns false when invalid hours', () => {
-        // arrange
-        const str = '25:51';
-        
-        // act
-        const result = validTime(str);
+    // log
+    console.log("result 1: ", result);
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(false);
-    });
+    // assert
+    expect(result).toBe(true);
+  });
 
-    it('returns false when invalid minutes', () => {
-        // arrange
-        const str = '02:76';
-        
-        // act
-        const result = validTime(str);
+  it("returns false when invalid hours", () => {
+    // arrange
+    const str = "25:51";
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(false);
-    });
+    // act
+    const result = validTime(str);
+
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe(false);
+  });
+
+  it("returns false when invalid minutes", () => {
+    // arrange
+    const str = "02:76";
+
+    // act
+    const result = validTime(str);
+
+    // log
+    console.log("result 1: ", result);
+
+    // assert
+    expect(result).toBe(false);
+  });
 });

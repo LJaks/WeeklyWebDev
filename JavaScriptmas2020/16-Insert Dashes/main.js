11bd0e41269d094b0a86a199d27fffbdb7c35787
Laura J <laura.jakseboge@gmail.com>
2020-12-17 18:46:10 +0100

    Split(), map(), join() methods;

---
+++ JavaScriptmas2020/16-Insert Dashes/main.js
@@ -0,0 +1,26 @@
+function insertDashes(arr) {
+  // write code here
+  return arr
+    .split(" ")
+    .map((x) => x.split("").join("-"))
+    .join(" ");
+}
+
+/**
+ * Test Suite
+ */
+describe("insertDashes()", () => {
+  it("insert dashes in between chars", () => {
+    // arrange
+    const value = "aba caba";
+
+    // act
+    const result = insertDashes(value);
+
+    // log
+    console.log("result: ", result);
+
+    // assert
+    expect(result).toBe("a-b-a c-a-b-a");
+  });
+});
